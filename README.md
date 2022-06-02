f9c966b2be658df7db0f66fb140aa4590598201a
Yokasta <yokaleis@gmail.com>
2022-06-02 15:09:25 -0400

    Funcion de descuento

--- README.md
+++ README.md
@@ -9,7 +9,5 @@
 ![Image text](https://github.com/Yokaleis/Curso-Practico-de-JavaScript-con-Platzi/blob/main/img/Captura%20web_1-6-2022_212429_.jpeg)
 
 # Segundo Taller:
-- Primer paso: Definir formulas
-- Segundo paso: Implementar las formulas en JS
-- Tercer paso: Crear las funciones
-- Cuarto paso: Integrar HTML con JS 
+- Primer paso: Definir formulas y crear las funciones 
+- Segundo paso: Escribir HTML desde JS 
--- descuentos.js
+++ descuentos.js
@@ -1,12 +1,23 @@
 const precioOriginal = 120;
 const descuento = 18;
-//Calculando descuento
-const porcentajePrecioConDescuento = 100 - descuento;
-const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
 
-console.log({
-    precioOriginal,
-    descuento, 
-    porcentajePrecioConDescuento,
-    precioConDescuento
-});
+
+//Calculando descuento
+function calcularPrecioConDescuento (precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function calcularDescuento() {
+    const inputPrecio    = parseInt(document.getElementById("InputPrecio").value);
+    const inputDescuento = parseInt(document.getElementById("InputDescuento").value);
+
+    const precioConDescuento = calcularPrecioConDescuento(inputPrecio, inputDescuento);
+
+    //Escribien en el HTML con JS
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerText = "El precio con descuento es: " + precioConDescuento;
+}
