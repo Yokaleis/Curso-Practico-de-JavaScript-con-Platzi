15cc4d3408e71ff3c5fc662e811ab34b82ffb794
Yokasta <yokaleis@gmail.com>
2022-06-02 19:06:03 -0400

    Aplicando cupones descuentos

--- descuentos.js
+++ descuentos.js
@@ -10,14 +10,33 @@ function calcularPrecioConDescuento (precio, descuento){
     return precioConDescuento;
 }
 
+const coupons = [
+    "HolaLove",
+    "JhonnyDeep",
+    "LOL",
+];
+
 function calcularDescuento() {
-    const inputPrecio    = parseInt(document.getElementById("InputPrecio").value);
-    const inputDescuento = parseInt(document.getElementById("InputDescuento").value);
+    const inputPrecio = parseInt(document.getElementById("InputPrecio").value);
+    const inputCoupon = document.getElementById("InputCoupon").value;
 
-    const precioConDescuento = calcularPrecioConDescuento(inputPrecio, inputDescuento);
+    let descuento;
+  
+    switch(inputCoupon) {
+      case coupons[0]: 
+        descuento = 15;
+      break;
+      case coupons[1]: 
+        descuento = 30;
+      break;
+      case coupons[2]: 
+        descuento = 50;
+      break;
+    }
 
-    //Escribien en el HTML con JS
-
+    const precioConDescuento = calcularPrecioConDescuento(inputPrecio, descuento);
+  
     const resultado = document.getElementById("resultado");
-    resultado.innerText = "El precio con descuento es: " + precioConDescuento;
-}
+    resultado.innerText = "El precio con descuento son: $" + precioConDescuento;
+
+  }
